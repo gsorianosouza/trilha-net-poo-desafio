202e982c45161d4ca4884616652b03e6ea81e34c
gsorianosouza <gabrielsorianosouza@outlook.com.br>
2024-10-23 13:52:24 -0300

    Resolução Projeto!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// FEITO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("NOKIA");
+Smartphone nokia = new Nokia(numero: "19981754632", modelo: "Modelo Nokia 2022", imei: "9999111", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+Console.WriteLine("====================");
+Console.WriteLine("\n");
+
+Console.WriteLine("IPHONE");
+Smartphone iphone = new Iphone(numero: "19952142631", modelo: "Iphone 15 Pro Max", imei: "88841111", memoria: 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
